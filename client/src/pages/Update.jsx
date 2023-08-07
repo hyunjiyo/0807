6134759bba2d288e47602e33dcfadd8766cc4198
hyunjiyo <hlee147@gmail.com>
2023-08-07 19:02:02 +0900

    first commit

---
+++ client/src/pages/Update.jsx
@@ -0,0 +1,50 @@
+/** @format */
+
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Update = () => {
+  const [book, setBook] = useState({
+    title: '',
+    desc: '',
+    price: null,
+    cover: '',
+  });
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  console.log(location);
+
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      // await axios.post('http://localhost:8800/books');
+      navigate('/');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log(book);
+
+  return (
+    <div className='form'>
+      <h1>Update new</h1>
+      <input type='text' placeholder='title' onChange={handleChange} name='title' />
+      <input type='text' placeholder='desc' onChange={handleChange} name='desc' />
+      <input type='number' placeholder='price' onChange={handleChange} name='price' />
+      <input type='text' placeholder='cover' onChange={handleChange} name='cover' />
+      <button className='formButton' onClick={handleClick}>
+        Update
+      </button>
+    </div>
+  );
+};
+
+export default Update;
